f4855e582998ac02a99ead390d2625b4e847e63d
gizem <gizemokatan86@gmail.com>
2025-05-14 18:58:07 -0400

    My first commit

---
+++ JavaScript_File.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!");
+
+var A = "This is a string";
+document.write(A);
+
+document.write ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\ <br> \"Eat my shorts!\" Bart responded.");
+        
+document.write ("\"Be who you are and say what you feel, " + " because those who mind don\'t matter and those who matter don\'t mind.\"" +"-Dr. Seuss");
+
+var B = "Concatenate" + "String"
+document.write(B)
+
+var Food = "Seasonal", Spring = "Pasta", Summer = "Salad", Fall = "Turkey", Winter = "Seafood";
+document.write(Spring);
+
+

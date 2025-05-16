f41f3cd20ed6795c7a9af90e71eaeee9d83f0992
gizem <114784169+gizemokatan@users.noreply.github.com>
2025-05-15 21:06:54 -0400

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello, World!"); //window.alert method
+
+var A = "This is a string"; //Value1
+document.write(A); //document.write method
+
+var B = "This is an another string"; // Value2
+document.write(B);
+
+document.write (A+B) // concatenate
+ 
+document.write (1986+2019) // expression
+
---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction (){
+    var sentence = "Click the box for appointment dates.";
+    sentence += " For our other locations, go back to the Main Menu."
+    document.getElementById ("Appointment").innerHTML = sentence // method that returns an element
+}
+
+
